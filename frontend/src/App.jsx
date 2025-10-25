913aeedcdc20e351cd1c408f6191de5c9c79eb38
SiAn20 <sidneyangellysg@gmail.com>
2025-10-25 11:01:32 -0400

    feat: Refactor App component to include routing and query client setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import "./App.css";
+import { useState } from "react";
+import Home from "./pages/home/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [client] = useState(() => new QueryClient());
   return (
-    <>
-      <div>
-        <h1>Clean Up 2025</h1>
-      </div>
-    </>
+    <div className="bg-gradient-to-br from-emerald-700 via-green-600 to-teal-700">
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/game" />
+            <Route path="/scanner" />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </div>
   );
 }
 
