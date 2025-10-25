d0d08e2846f385357b372172d88eb72ec861b576
link200309 <jonhh.0023@gmail.com>
2025-10-25 09:46:33 -0400

    feat: Add Login and Register page

---
+++ frontend/src/features/auth/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import AuthLayout from "../../../shared/layout/AuthLayout";
+import LoginForm from "../components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
+  );
+};
+
+export default LoginPage;
