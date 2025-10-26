32d50744c6aa53083665ef95440d123d25d91853
SiAn20 <sidneyangellysg@gmail.com>
2025-10-26 00:26:58 -0400

    feat: Refactor HomeView component to include Header and TrashBin; update layout and styles

--- frontend/src/features/learn/pages/Learn.jsx
+++ frontend/src/features/learn/pages/Learn.jsx
@@ -9,7 +9,7 @@ function Learn() {
   const [selectedModule, setSelectedModule] = useState(null);
 
   return (
-    <div className="min-h-screen pt-10">
+    <div className="min-h-screen pt-5">
       {currentView === "home" && (
         <HomeView
           setCurrentView={setCurrentView}
